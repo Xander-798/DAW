5eacefdd407e5a34028675cb76f4dd0dc6738a65
Xander-798 <71306528+Xander-798@users.noreply.github.com>
2022-07-07 18:40:20 -0600

    Add files via upload

---
+++ guia 1/Discusión de Resultados/ejercicio complementario - 1/js/primer.js
@@ -0,0 +1,12 @@
+document.write("<header><h2>Header</h2></header>");
+document.write("<section>");
+document.write("<aside><nav><h2>Navigation</h2></nav></aside>");
+document.write("<div>");
+document.write("<article><h2>Article</h2></article>");
+document.write("<article><h2>Article</h2></article>");
+document.write("<article><h2>Article</h2></article>");
+document.write("</div>");
+document.write("</section>");
+document.write("<footer><h2>Footer</h2></footer>");
+
+
